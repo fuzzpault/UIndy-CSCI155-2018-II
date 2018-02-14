1316f4565f77b08c1f265a83c9f6d074f1bf822d
Paul Talaga <talagap@uindy.edu>
2018-02-14 02:11:41 +0000

    Find the errors!

---
+++ ClassExamples/Week05-Conditionals2/syntax-errors.c
@@ -0,0 +1,19 @@
+#define <stdioh>
+
+int mymain()(
+	
+	scanf("Give me a number %d", number)
+	
+	printf("2 * %f = %f\", number, 2 x number);
+	
+	if(5 < number < 15){
+		printf('Your number is greater than 5 and less than 15")
+	}else(number < 5){
+		printf("Your number is less than 5");
+	}else(number > 15){
+		printf('Your number is greater than 15');
+	}
+	
+	printf("Thanks!  Have a good day! %d\n");
+	
+)
