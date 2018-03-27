a5ccb0eba89ec93d0fea198216456ec8baa25965
Paul Talaga <talagap@uindy.edu>
2018-03-27 13:47:29 +0000

    Example

---
+++ ClassExamples/Week10-Arrays2/BadNumberAnalysis1.c
@@ -0,0 +1,61 @@
+/*
+	Name: Paul Talaga
+	Date: Mar 20, 2018
+	Desc: Bad number analysis
+*/
+
+#include <stdio.h>
+#define MAX_SIZE 50
+
+int main(){
+	int choice = 0;
+	
+	int values[MAX_SIZE];	
+	int current_size = 0;  
+	int i = 0;
+	
+	while(choice != 0){
+		printf("\nWhat do you want to do?\n")
+		printf("0. Exit\n")
+		printf("1. Enter values\n")
+		printf("2. IsSorted?\n")
+		printf("3. Print List\n")
+		
+		scanf("%d", &choice);
+		while(choice < 1 && choice > 2){
+			printf("Enter a valid choice!\n");
+		}
+		
+		if(choice == 1){ // Enter values
+			printf("Enter up to %d values, or -99 to stop.", MAX_SIZE)
+			current_size = 0; // Reset - start from beginning
+			for(i = 0; i < MAX_SIZE; i++){
+				int temp = 0;
+				scanf("%d", &temp );
+				if(temp == -99){
+					current_size = i;
+					break;
+				}
+				values[i] = temp;
+			} 
+		}else if(choice == 2){  // IsSorted
+			int isSorted = 1;
+			for(i = 0, i < current_size - 1, i++){
+				if(values[i] > values[i + 1]){
+					isSorted = 0;
+				}
+			}
+			if(isSorted){
+				print("The numbers are sorted!\n");
+			}else{
+				printf("The numbers are NOT sorted!\n");
+			}
+		}else if(choice == 3){  // Print
+			for(i = 1, i <= current_size, i++){
+				printf("%d ", values[i]);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
